c413a66c79487600dea16e2e55a3753b065c1d82
何方 <he@hefang.link>
2020-03-08 10:06:49 +0800

    添加ts依赖, 更改返回值类型声明

--- index.ts
+++ index.ts
@@ -1,25 +1,25 @@
-export function getLocalStorage<T>(name: string, defaultValue: T = null): T | null {
-    try {
-        const val = localStorage.getItem(name);
-        return val ? JSON.parse(val) : defaultValue;
-    } catch (e) {
-        return defaultValue;
-    }
+export function getLocalStorage<T>(name: string, defaultValue: T = null): T {
+	try {
+		const val = localStorage.getItem(name);
+		return val ? JSON.parse(val) : defaultValue;
+	} catch (e) {
+		return defaultValue;
+	}
 }
 
 export function setLocalStorage(name: string, value: any) {
-    localStorage.setItem(name, JSON.stringify(value))
+	localStorage.setItem(name, JSON.stringify(value))
 }
 
-export function getSessionStorage<T>(name: string, defaultValue: T = null): T | null {
-    try {
-        const val = sessionStorage.getItem(name);
-        return val ? JSON.parse(val) : defaultValue;
-    } catch (e) {
-        return defaultValue;
-    }
+export function getSessionStorage<T>(name: string, defaultValue: T = null): T {
+	try {
+		const val = sessionStorage.getItem(name);
+		return val ? JSON.parse(val) : defaultValue;
+	} catch (e) {
+		return defaultValue;
+	}
 }
 
 export function setSessionStorage(name: string, value: any) {
-    sessionStorage.setItem(name, JSON.stringify(value))
+	sessionStorage.setItem(name, JSON.stringify(value))
 }
